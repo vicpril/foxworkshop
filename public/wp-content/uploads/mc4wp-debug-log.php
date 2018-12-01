913a4d7f32059cfb4871a7a4f1171e982b6e729b
Viktor Prilepin <vicpride@gmail.com>
2018-12-01 18:10:44 +0700

    01/12/18

---
+++ public/wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,3 @@
+<?php exit; ?>
+[2018-12-01 16:07:12] WARNING: Form 119 > aida***@ma**.ru is already subscribed to the selected list(s)
+[2018-12-01 16:10:36] WARNING: Form 119 > aida***@ma**.ru is already subscribed to the selected list(s)
